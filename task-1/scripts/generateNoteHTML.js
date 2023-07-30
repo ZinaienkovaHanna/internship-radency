accf056a2b12a1f6b848e252baaebd5f9f933215
ZinaienkovaHanna <zinaienkova.anna@gmail.com>
2023-07-30 11:47:08 +0300

    add notes.css, button.css

--- task-1/scripts/generateNoteHTML.js
+++ task-1/scripts/generateNoteHTML.js
@@ -1,15 +1,19 @@
 function generateNoteHTML(note, index) {
     return `
       <tr>
-        <td><i class="fas fa-${note.icon}"></i>${note.name}</td>
+        <td class="circle-icon-flex">
+          <div class="circle-icon">
+            <i class="fas fa-${note.icon}"></i>
+          </div>${note.name}
+        </td>
         <td>${note.created}</td>
         <td>${note.category}</td>
         <td>${note.content}</td>
         <td>${note.dates}</td>
         <td>
-          <i class="fas fa-edit edit-note-btn" data-index="${index}"></i>
-          <i class="fas fa-archive"></i>
-          <i class="fas fa-trash delete-note-btn" data-index="${index}"></i>
+          <i class="fas fa-edit edit-note-btn note" data-index="${index}"></i>
+          <i class="fas fa-archive note"></i>
+          <i class="fas fa-trash delete-note-btn note" data-index="${index}"></i>
         </td>
       </tr>
     `;
